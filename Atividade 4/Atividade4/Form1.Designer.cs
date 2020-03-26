533caa31b591d775aee78cb6b2307520c61c09cd
Rafael Pacos <rafapacos@gmail.com>
2020-03-26 13:27:49 -0300

    Atividade 4 LP2

---
+++ Atividade 4/Atividade4/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace Atividade4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblSal = new System.Windows.Forms.Label();
+            this.lblFilhos = new System.Windows.Forms.Label();
+            this.lblINSS = new System.Windows.Forms.Label();
+            this.lblIRPF = new System.Windows.Forms.Label();
+            this.lblSalFam = new System.Windows.Forms.Label();
+            this.lblSalLiq = new System.Windows.Forms.Label();
+            this.lblDescINSS = new System.Windows.Forms.Label();
+            this.lblDescIRPF = new System.Windows.Forms.Label();
+            this.txtbFunc = new System.Windows.Forms.MaskedTextBox();
+            this.txtbSalB = new System.Windows.Forms.MaskedTextBox();
+            this.txtbFilhos = new System.Windows.Forms.MaskedTextBox();
+            this.txtbINSS = new System.Windows.Forms.MaskedTextBox();
+            this.txtbIRPF = new System.Windows.Forms.MaskedTextBox();
+            this.txtbSalFam = new System.Windows.Forms.MaskedTextBox();
+            this.txtbSalLiq = new System.Windows.Forms.MaskedTextBox();
+            this.txtbDescIRPF = new System.Windows.Forms.MaskedTextBox();
+            this.txtbDescINSS = new System.Windows.Forms.MaskedTextBox();
+            this.grpbSexo = new System.Windows.Forms.GroupBox();
+            this.rdbtFem = new System.Windows.Forms.RadioButton();
+            this.rdbtMasc = new System.Windows.Forms.RadioButton();
+            this.btnVerifDesc = new System.Windows.Forms.Button();
+            this.checkbCasado = new System.Windows.Forms.CheckBox();
+            this.lblDados = new System.Windows.Forms.Label();
+            this.grpbSexo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblNome
+            // 
+            resources.ApplyResources(this.lblNome, "lblNome");
+            this.lblNome.Name = "lblNome";
+            // 
+            // lblSal
+            // 
+            resources.ApplyResources(this.lblSal, "lblSal");
+            this.lblSal.Name = "lblSal";
+            // 
+            // lblFilhos
+            // 
+            resources.ApplyResources(this.lblFilhos, "lblFilhos");
+            this.lblFilhos.Name = "lblFilhos";
+            // 
+            // lblINSS
+            // 
+            resources.ApplyResources(this.lblINSS, "lblINSS");
+            this.lblINSS.Name = "lblINSS";
+            // 
+            // lblIRPF
+            // 
+            resources.ApplyResources(this.lblIRPF, "lblIRPF");
+            this.lblIRPF.Name = "lblIRPF";
+            // 
+            // lblSalFam
+            // 
+            resources.ApplyResources(this.lblSalFam, "lblSalFam");
+            this.lblSalFam.Name = "lblSalFam";
+            // 
+            // lblSalLiq
+            // 
+            resources.ApplyResources(this.lblSalLiq, "lblSalLiq");
+            this.lblSalLiq.Name = "lblSalLiq";
+            // 
+            // lblDescINSS
+            // 
+            resources.ApplyResources(this.lblDescINSS, "lblDescINSS");
+            this.lblDescINSS.Name = "lblDescINSS";
+            // 
+            // lblDescIRPF
+            // 
+            resources.ApplyResources(this.lblDescIRPF, "lblDescIRPF");
+            this.lblDescIRPF.Name = "lblDescIRPF";
+            this.lblDescIRPF.Click += new System.EventHandler(this.lblDescIRPF_Click);
+            // 
+            // txtbFunc
+            // 
+            resources.ApplyResources(this.txtbFunc, "txtbFunc");
+            this.txtbFunc.Name = "txtbFunc";
+            // 
+            // txtbSalB
+            // 
+            resources.ApplyResources(this.txtbSalB, "txtbSalB");
+            this.txtbSalB.Name = "txtbSalB";
+            // 
+            // txtbFilhos
+            // 
+            resources.ApplyResources(this.txtbFilhos, "txtbFilhos");
+            this.txtbFilhos.Name = "txtbFilhos";
+            // 
+            // txtbINSS
+            // 
+            resources.ApplyResources(this.txtbINSS, "txtbINSS");
+            this.txtbINSS.Name = "txtbINSS";
+            // 
+            // txtbIRPF
+            // 
+            resources.ApplyResources(this.txtbIRPF, "txtbIRPF");
+            this.txtbIRPF.Name = "txtbIRPF";
+            // 
+            // txtbSalFam
+            // 
+            resources.ApplyResources(this.txtbSalFam, "txtbSalFam");
+            this.txtbSalFam.Name = "txtbSalFam";
+            // 
+            // txtbSalLiq
+            // 
+            resources.ApplyResources(this.txtbSalLiq, "txtbSalLiq");
+            this.txtbSalLiq.Name = "txtbSalLiq";
+            // 
+            // txtbDescIRPF
+            // 
+            resources.ApplyResources(this.txtbDescIRPF, "txtbDescIRPF");
+            this.txtbDescIRPF.Name = "txtbDescIRPF";
+            // 
+            // txtbDescINSS
+            // 
+            resources.ApplyResources(this.txtbDescINSS, "txtbDescINSS");
+            this.txtbDescINSS.Name = "txtbDescINSS";
+            // 
+            // grpbSexo
+            // 
+            this.grpbSexo.Controls.Add(this.rdbtFem);
+            this.grpbSexo.Controls.Add(this.rdbtMasc);
+            resources.ApplyResources(this.grpbSexo, "grpbSexo");
+            this.grpbSexo.Name = "grpbSexo";
+            this.grpbSexo.TabStop = false;
+            // 
+            // rdbtFem
+            // 
+            resources.ApplyResources(this.rdbtFem, "rdbtFem");
+            this.rdbtFem.Name = "rdbtFem";
+            this.rdbtFem.TabStop = true;
+            this.rdbtFem.UseVisualStyleBackColor = true;
+            // 
+            // rdbtMasc
+            // 
+            resources.ApplyResources(this.rdbtMasc, "rdbtMasc");
+            this.rdbtMasc.Name = "rdbtMasc";
+            this.rdbtMasc.TabStop = true;
+            this.rdbtMasc.UseVisualStyleBackColor = true;
+            // 
+            // btnVerifDesc
+            // 
+            resources.ApplyResources(this.btnVerifDesc, "btnVerifDesc");
+            this.btnVerifDesc.Name = "btnVerifDesc";
+            this.btnVerifDesc.UseVisualStyleBackColor = true;
+            this.btnVerifDesc.Click += new System.EventHandler(this.btnVerifDesc_Click);
+            // 
+            // checkbCasado
+            // 
+            resources.ApplyResources(this.checkbCasado, "checkbCasado");
+            this.checkbCasado.Name = "checkbCasado";
+            this.checkbCasado.UseVisualStyleBackColor = true;
+            // 
+            // lblDados
+            // 
+            resources.ApplyResources(this.lblDados, "lblDados");
+            this.lblDados.BackColor = System.Drawing.Color.White;
+            this.lblDados.Name = "lblDados";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lblDados);
+            this.Controls.Add(this.checkbCasado);
+            this.Controls.Add(this.btnVerifDesc);
+            this.Controls.Add(this.grpbSexo);
+            this.Controls.Add(this.txtbDescINSS);
+            this.Controls.Add(this.txtbDescIRPF);
+            this.Controls.Add(this.txtbSalLiq);
+            this.Controls.Add(this.txtbSalFam);
+            this.Controls.Add(this.txtbIRPF);
+            this.Controls.Add(this.txtbINSS);
+            this.Controls.Add(this.txtbFilhos);
+            this.Controls.Add(this.txtbSalB);
+            this.Controls.Add(this.txtbFunc);
+            this.Controls.Add(this.lblDescIRPF);
+            this.Controls.Add(this.lblDescINSS);
+            this.Controls.Add(this.lblSalLiq);
+            this.Controls.Add(this.lblSalFam);
+            this.Controls.Add(this.lblIRPF);
+            this.Controls.Add(this.lblINSS);
+            this.Controls.Add(this.lblFilhos);
+            this.Controls.Add(this.lblSal);
+            this.Controls.Add(this.lblNome);
+            this.Name = "Form1";
+            this.grpbSexo.ResumeLayout(false);
+            this.grpbSexo.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblSal;
+        private System.Windows.Forms.Label lblFilhos;
+        private System.Windows.Forms.Label lblINSS;
+        private System.Windows.Forms.Label lblIRPF;
+        private System.Windows.Forms.Label lblSalFam;
+        private System.Windows.Forms.Label lblSalLiq;
+        private System.Windows.Forms.Label lblDescINSS;
+        private System.Windows.Forms.Label lblDescIRPF;
+        private System.Windows.Forms.MaskedTextBox txtbFunc;
+        private System.Windows.Forms.MaskedTextBox txtbSalB;
+        private System.Windows.Forms.MaskedTextBox txtbFilhos;
+        private System.Windows.Forms.MaskedTextBox txtbINSS;
+        private System.Windows.Forms.MaskedTextBox txtbIRPF;
+        private System.Windows.Forms.MaskedTextBox txtbSalFam;
+        private System.Windows.Forms.MaskedTextBox txtbSalLiq;
+        private System.Windows.Forms.MaskedTextBox txtbDescIRPF;
+        private System.Windows.Forms.MaskedTextBox txtbDescINSS;
+        private System.Windows.Forms.GroupBox grpbSexo;
+        private System.Windows.Forms.RadioButton rdbtFem;
+        private System.Windows.Forms.RadioButton rdbtMasc;
+        private System.Windows.Forms.Button btnVerifDesc;
+        private System.Windows.Forms.CheckBox checkbCasado;
+        private System.Windows.Forms.Label lblDados;
+    }
+}
+
---
+++ Atividade 4/Atividade4/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Atividade4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lblDescIRPF_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnVerifDesc_Click(object sender, EventArgs e)
+        {
+            double dSalBruto, dDescINSS, dSalLiq, dDescIRPF = 0.00, dSalFam = 0.00;
+            int iFilhos;
+
+            if (txtbFunc.Text == "" || double.TryParse(txtbFunc.Text, out dSalBruto) == true)
+            {
+                MessageBox.Show("Entrar com Nome Correto");
+            }
+            else
+            {
+                if (double.TryParse(txtbSalB.Text, out dSalBruto) && int.TryParse(txtbFilhos.Text, out iFilhos))
+                {
+                    dSalBruto = dSalBruto / 100;
+                    if (dSalBruto <= 800.47)
+                    {
+                        dDescINSS = 0.0765 * dSalBruto;
+                        txtbINSS.Text = "7.65%";
+                    }
+                    else if (dSalBruto <= 1050)
+                    {
+                        dDescINSS = 0.0865 * dSalBruto;
+                        txtbINSS.Text = "8.65%";
+                    }
+                    else if (dSalBruto <= 1400.77)
+                    {
+                        dDescINSS = 0.09 * dSalBruto;
+                        txtbINSS.Text = "9.00%";
+                    }
+                    else if (dSalBruto <= 2801.56)
+                    {
+                        dDescINSS = 0.11 * dSalBruto;
+                        txtbINSS.Text = "11.%";
+                    }
+                    else
+                    {
+                        dDescINSS = 308.17;
+                        txtbINSS.Text = "Teto R$ " + dDescINSS.ToString();
+                    }
+
+                    if (dSalBruto <= 1257.12)
+                    {
+                        txtbIRPF.Text = "Isento";
+                    }
+                    else if (dSalBruto <= 2508.12)
+                    {
+                        dDescIRPF = 0.15;
+                        txtbIRPF.Text = (dDescIRPF * 100).ToString() + "%";
+
+                    }
+                    else
+                    {
+                        dDescIRPF = 0.275;
+                        txtbIRPF.Text = (dDescIRPF * 100).ToString() + "%";
+                    }
+                    if (dSalBruto <= 435.52)
+                    {
+                        dSalFam = 22.33 * iFilhos;
+                    }
+                    dSalLiq = dSalBruto - dDescINSS - dDescIRPF * dSalBruto + dSalFam;
+                    txtbSalLiq.Text = Math.Round(dSalLiq, 2).ToString("N2");
+                    txtbSalFam.Text = dSalFam.ToString("N2");
+                    txtbDescINSS.Text = dDescINSS.ToString("N2");
+                    txtbDescIRPF.Text = (dDescIRPF * dSalBruto).ToString("N2");
+                    if (rdbtMasc.Checked)
+                    {
+                        if (checkbCasado.Checked)
+                        {
+                            lblDados.Text = "Os desconto do Salário do Sr." + txtbFunc.Text + "\n" +
+                                "que é casado e que tem " + iFilhos.ToString() + " filho(s) são: ";
+                        }
+                        else
+                        {
+                            lblDados.Text = "Os desconto do Salário do Sra." + txtbFunc.Text + "\n" +
+                                "que é solteiro e que tem " + iFilhos.ToString() + " filho(s) são: ";
+                        }
+
+                    }
+                    else if (rdbtFem.Checked)
+                    {
+                        if (checkbCasado.Checked)
+                        {
+                            lblDados.Text = "Os desconto do Salário do Sr." + txtbFunc.Text + "\n" +
+                                "que é casada e que tem " + iFilhos.ToString() + " filho(s) são: ";
+                        }
+                        else
+                        {
+                            lblDados.Text = "Os desconto do Salário do Sra." + txtbFunc.Text + "\n" +
+                                "que é solteira e que tem " + iFilhos.ToString() + " filho(s) são: ";
+                        }
+                    }
+                    else
+                        MessageBox.Show("Marcar o Sexo !");
+                }
+                else
+                    MessageBox.Show("Preencher Dados Válidos");
+            }    
+        }
+    }
+}
