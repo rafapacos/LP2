596c22dc0ea8f547a67fd262b32bc985ed264497
Rafael Pacos <rafapacos@gmail.com>
2020-06-19 21:23:11 -0300

    Prova

---
+++ P30481923042/P30481923042/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace P30481923042 {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.lboxFaturamentos = new System.Windows.Forms.ListBox();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lboxFaturamentos
+            // 
+            this.lboxFaturamentos.FormattingEnabled = true;
+            this.lboxFaturamentos.Location = new System.Drawing.Point(480, 17);
+            this.lboxFaturamentos.Name = "lboxFaturamentos";
+            this.lboxFaturamentos.Size = new System.Drawing.Size(308, 420);
+            this.lboxFaturamentos.TabIndex = 0;
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(114, 156);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(154, 99);
+            this.btnVerificar.TabIndex = 1;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnVerificar);
+            this.Controls.Add(this.lboxFaturamentos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lboxFaturamentos;
+        private System.Windows.Forms.Button btnVerificar;
+    }
+}
+
---
+++ P30481923042/P30481923042/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using System.Collections;
+
+namespace P30481923042 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e) {
+            lboxFaturamentos.Items.Clear();
+            double[,] Faturamentos = new double[2, 4];
+            string[,] strFaturamentos = new string[2, 4];
+            double[] FaturamentoMes = new double[2];
+            string[] aux = new string[4];
+            int cont = 0;
+            double TotGeral = 0.00;
+
+            for ( var i = 0; i < 2; i++) {
+                for(var j = 0; j < 4; j++) {
+                    do {
+                        strFaturamentos[i, j] = Interaction.InputBox("Digite o faturamento da semana " + (j+1) + " do mês " + (i+1)
+                            , "Entrada dos Dados");
+                    } while (!double.TryParse(strFaturamentos[i, j], out Faturamentos[i, j]));
+                    aux[j] = "Total do mês " + (i + 1) + " Semana " + (j + 1) + " R$ " + Faturamentos[i, j].ToString("F2") + "\n";
+                    lboxFaturamentos.Items.Insert(cont, aux[j]);
+                    cont++;
+
+                }
+                FaturamentoMes[i] = Faturamentos[i, 0] + Faturamentos[i, 1] + Faturamentos[i, 2] + Faturamentos[i, 3];
+                MessageBox.Show(FaturamentoMes[i].ToString("f2"));
+                lboxFaturamentos.Items.Insert(cont, ">> Total Mês R$ " + FaturamentoMes[i].ToString("F2"));
+                cont++;
+                lboxFaturamentos.Items.Insert((cont), "--------------------------------------------");
+                cont++;
+                TotGeral += FaturamentoMes[i];
+
+            }
+            lboxFaturamentos.Items.Insert(cont, ">> Total Geral: R$ " + TotGeral.ToString("F2"));
+
+
+        }
+    }
+}
